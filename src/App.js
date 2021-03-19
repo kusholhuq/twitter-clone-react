29831ddb135cb2e996448df2f53e33e03d6d6f75
Mohsinul Huq <kusholhuq@gmail.com>
2021-03-18 21:01:00 -0700

    sidebar options added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import Sidebar from './components/Sidebar'
 
 function App() {
   return (
     //BEM
     <div className="app">
       <h1>Twitter Clone</h1>
+
+      {/*  Sidebar   */}
+      <Sidebar/>
+
+      {/*  Feed   */}
+      {/*  Widgets   */}
+
+
     </div>
   );
 }
