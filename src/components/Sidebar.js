d99c1fa652d1a7716b7b98e5d612aefecbc0574b
Mohsinul Huq <kusholhuq@gmail.com>
2021-03-18 17:32:25 -0700

    sidebar component created

---
+++ src/components/Sidebar.js
@@ -0,0 +1,22 @@
+import React from "react";
+import '../styles/Sidebar.css'
+
+function Sidebar() {
+  return(
+    <div className="sidebar">
+      {/* Twitter Icon */}
+
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+
+      {/* Button --> Tweet */}
+    </div>
+  )
+}
+
+export default Sidebar;
