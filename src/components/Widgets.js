d57314838e16bfb3c21ba85dd6f1c90ce3ecfef4
Mohsinul Huq <kusholhuq@gmail.com>
2021-03-19 16:28:22 -0700

    react twitter embed installed

--- src/components/Widgets.js
+++ src/components/Widgets.js
@@ -1,10 +1,31 @@
 import React from 'react';
-import "../styles/Widgets.css"
+import "../styles/Widgets.css";
+import SearchIcon from '@material-ui/icons/Search';
+import {
+  TwitterTimelineEmbed,
+  TwitterShareButton,
+  TwitterTweetEmbed,
+} from "react-twitter-embed";
 
 function Widgets () {
   return (
-    <div>
-      <h2>Widgets</h2>
+    <div className="widgets">
+
+      <div className="widgets__input">
+        <SearchIcon className="widgets__searchIcon"/>
+        <input placeholder="Search Twitter" type="text" />
+      </div>
+
+      <div className="widgets__widgetContainer">
+        <h2>What's happening</h2>
+        <TwitterTweetEmbed tweetId={"858551177860055040"}/>
+        <TwitterTimelineEmbed
+          sourceType="profile"
+          screenName="cleverqazi"
+          options={{height:400}}
+        />
+      </div>
+
     </div>
   )
 }
