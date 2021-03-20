746084d9c4837abbdc938e15f79ca19f8b0de97e
Mohsinul Huq <kusholhuq@gmail.com>
2021-03-19 19:17:30 -0700

    started working with firebase

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAUzvPiWt6fVulYBpTjwO49kGJ1791v9nQ",
+  authDomain: "zilp-8807e.firebaseapp.com",
+  projectId: "zilp-8807e",
+  storageBucket: "zilp-8807e.appspot.com",
+  messagingSenderId: "288331005914",
+  appId: "1:288331005914:web:828c5d7a555128c4cc8ae3",
+  measurementId: "G-R8P6XD5H93"
+};
