20325e8521c2e44bfea298581e2b324d9e96d2a6
Mohsinul Huq <kusholhuq@gmail.com>
2021-03-20 01:48:54 -0700

    imported firebase and started db setup

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,6 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyAUzvPiWt6fVulYBpTjwO49kGJ1791v9nQ",
   authDomain: "zilp-8807e.firebaseapp.com",
@@ -8,3 +10,8 @@ const firebaseConfig = {
   appId: "1:288331005914:web:828c5d7a555128c4cc8ae3",
   measurementId: "G-R8P6XD5H93"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+
+export default db;
